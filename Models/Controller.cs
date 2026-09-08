5a5f6ebc980017659693ebc0bad38415afa1959f
Utso Roy <royutso003@gmail.com>
2026-09-08 12:26:45 +0600

    Practice to MVC architecture

---
+++ Models/Controller.cs
@@ -0,0 +1,16 @@
+namespace Name.Controllers
+{
+
+    using Microsoft.AspNetCore.Mvc;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            
+            return Ok();
+        }
+    }
+}
